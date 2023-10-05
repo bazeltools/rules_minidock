17dfda89c567e718b8603818d23baf7b16e15693
Rob Story <rstory@netflix.com>
2023-10-05 16:19:05 -0700

    Test env as well, fix CI

--- scripts/ci_test.py
+++ scripts/ci_test.py
@@ -5,26 +5,27 @@ import os
 import subprocess
 
 # Navigate to tests/simple_flow
-os.chdir('tests/simple_flow')
+os.chdir("tests/simple_flow")
 
 # Run bazel clean --expunge
-subprocess.run(['bazel', 'clean', '--expunge'], check=True)
+subprocess.run(["bazel", "clean", "--expunge"], check=True)
 
 # Run bazel build ...
-subprocess.run(['bazel', 'build', '...'], check=True)
+subprocess.run(["bazel", "build", "..."], check=True)
 
 # Calculate SHA_A
-with open('bazel-bin/test_assemble_simple_merger_manifest.json', 'rb') as f:
-    SHA_A = subprocess.run(['shasum', '-a', '256'], stdin=f, capture_output=True, check=True).stdout.strip()
+with open("bazel-bin/test_assemble_simple_merger_manifest.json", "rb") as f:
+    SHA_A = subprocess.run(["shasum", "-a", "256"], stdin=f, capture_output=True, check=True).stdout.strip()
 
 # Load JSON file
-with open('bazel-bin/test_assemble_simple_merger_config.json') as json_file:
+with open("bazel-bin/test_assemble_simple_merger_config.json") as json_file:
     data = json.load(json_file)
 
 # Check Cmd, Labels, and User fields
-cmd = data['config']['Cmd']
-labels = data['config']['Labels']
-user = data['config']['User']
+cmd = data["config"]["Cmd"]
+labels = data["config"]["Labels"]
+user = data["config"]["User"]
+env = data["config"]["Env"]
 
 # Expected values
 expected_cmd = ["/usr/bin/my_app"]
@@ -34,16 +35,20 @@ expected_user = "nfbasic"
 assert(cmd == expected_cmd)
 assert(labels == expected_labels)
 assert(user == expected_user)
+# We should not override the env, we should add to the existing env
+assert(len(env) > 2)
+assert("ENV1=FOO" in env)
+assert("ENV2=BAR" in env)
 
 # Run bazel clean --expunge
-subprocess.run(['bazel', 'clean', '--expunge'], check=True)
+subprocess.run(["bazel", "clean", "--expunge"], check=True)
 
 # Run bazel build ...
-subprocess.run(['bazel', 'build', '...'], check=True)
+subprocess.run(["bazel", "build", "..."], check=True)
 
 # Calculate SHA_B
-with open('bazel-bin/test_assemble_simple_merger_manifest.json', 'rb') as f:
-    SHA_B = subprocess.run(['shasum', '-a', '256'], stdin=f, capture_output=True, check=True).stdout.strip()
+with open("bazel-bin/test_assemble_simple_merger_manifest.json", "rb") as f:
+    SHA_B = subprocess.run(["shasum", "-a", "256"], stdin=f, capture_output=True, check=True).stdout.strip()
 
 # Compare SHA_A and SHA_B
 if SHA_A != SHA_B:
@@ -55,7 +60,7 @@ else:
     print(f"[ok] SHA256 matched {SHA_A}")
 
 # Navigate to tests/override_tools
-os.chdir('../../tests/override_tools')
+os.chdir("../../tests/override_tools")
 
 # Run bazel query ...
-subprocess.run(['bazel', 'query', '...'], check=True)
+subprocess.run(["bazel", "query", "..."], check=True)
