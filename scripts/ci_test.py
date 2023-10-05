2e6dbe5ed26fe2f1259d073a60f2193e374e52b2
Rob Story <rstory@netflix.com>
2023-10-05 16:20:00 -0700

    Clean up comments

--- scripts/ci_test.py
+++ scripts/ci_test.py
@@ -4,30 +4,22 @@ import json
 import os
 import subprocess
 
-# Navigate to tests/simple_flow
 os.chdir("tests/simple_flow")
 
-# Run bazel clean --expunge
 subprocess.run(["bazel", "clean", "--expunge"], check=True)
-
-# Run bazel build ...
 subprocess.run(["bazel", "build", "..."], check=True)
 
-# Calculate SHA_A
 with open("bazel-bin/test_assemble_simple_merger_manifest.json", "rb") as f:
     SHA_A = subprocess.run(["shasum", "-a", "256"], stdin=f, capture_output=True, check=True).stdout.strip()
 
-# Load JSON file
 with open("bazel-bin/test_assemble_simple_merger_config.json") as json_file:
     data = json.load(json_file)
 
-# Check Cmd, Labels, and User fields
 cmd = data["config"]["Cmd"]
 labels = data["config"]["Labels"]
 user = data["config"]["User"]
 env = data["config"]["Env"]
 
-# Expected values
 expected_cmd = ["/usr/bin/my_app"]
 expected_labels = {"label1": "foo", "label2": "bar"}
 expected_user = "nfbasic"
@@ -40,17 +32,12 @@ assert(len(env) > 2)
 assert("ENV1=FOO" in env)
 assert("ENV2=BAR" in env)
 
-# Run bazel clean --expunge
 subprocess.run(["bazel", "clean", "--expunge"], check=True)
-
-# Run bazel build ...
 subprocess.run(["bazel", "build", "..."], check=True)
 
-# Calculate SHA_B
 with open("bazel-bin/test_assemble_simple_merger_manifest.json", "rb") as f:
     SHA_B = subprocess.run(["shasum", "-a", "256"], stdin=f, capture_output=True, check=True).stdout.strip()
 
-# Compare SHA_A and SHA_B
 if SHA_A != SHA_B:
     print("expected same sha but got")
     print(SHA_A)
